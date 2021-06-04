8508130b026e9b8012bcea9c667ee5b1a48763ac
Thiago Reis <thiagoreis93@gmail.com>
2021-06-04 19:13:20 -0300

    chore: add product on cart, update cart items amount and get products from api json server

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +29,48 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
+  useEffect(() => {
+    localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+  }, [cart]);
+
   const addProduct = async (productId: number) => {
     try {
+      const response = await api.get(`/products/${productId}`);
+      const product = response.data;
+
+      let productExist = false;
+      const updatedCart = cart.map((cartItem) => {
+        if (cartItem.id === productId) {
+          productExist = true;
+          return {
+            ...cartItem,
+            amount: cartItem.amount + 1,
+          };
+        } else {
+          return cartItem;
+        }
+      });
+
+      if (productExist) {
+        setCart(updatedCart);
+      } else {
+        const newProduct = {
+          ...product,
+          amount: 1,
+        };
+        setCart([...updatedCart, newProduct]);
+      }
+    } catch (err) {
       // TODO
-    } catch {
-      // TODO
+      console.log(err);
     }
   };
 
